5ce8a7228b70c316ef9d183ddc1d60db7ee158f9
Steffen Tiedemann Christensen <steffen@refresh.dk>
2012-03-09 09:07:56 +0100

    Client-side library for 23 Video

---
+++ README.md
@@ -0,0 +1,34 @@
+## 23 Video API for JavaScript and jQuery
+
+`visualplatform.js` is a client-side JavaScript library for the read-only parts of the [The 23 Video API](http://www.23developer.com/api) (or more correctly, The Visualplatform API).
+
+The library is designed to work across domains using [JSON-P](http://www.23developer.com/api/#response-formats) and it requires jQuery.
+
+## Using the library
+
+Make sure the library is including the HTML page:
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script src="visualplatform.js"></script>
+
+Making simple request to the open API:
+
+    var visualplatform = Visualplatform(yourDomain);
+    visualplatform.album.list(
+      {search:'test'}, 
+      function(data){...}
+      function(errorMessage){...}
+    );
+
+Methods can be called as:
+
+    visualplatform.photo.photoSectionList(...)
+    visualplatform['photo.photoSectionList'](...)
+    visualplatform['/api/photo/section/list'](...)
+
+
+The first parameter is always a JS object with the filter data described in [the API documentation](http://www.23developer.com/api/#methods). The second and third parameters are callbacks in the event of success and error respectively.
+
+## Client vs Server.
+
+The client-side JavaScript library does not support OAuth authentication and it does not allow for any method that requires [`write`, `admin` or `super` privilges](http://www.23developer.com/api/#permission-levels). For this refer to the [Node.js library](http://github.com/23/node-23video) for the server side.
---
+++ visualplatform.js
@@ -0,0 +1,88 @@
+/*
+var visualplatform = Visualplatform(domain);
+visualplatform.album.list(
+  {search:'test'}, 
+  function(data){...}
+  function(errorMessage){...}
+);
+
+Methods can be called as:
+  visualplatform.photo.photoSectionList(...)
+  visualplatform['photo.photoSectionList'](...)
+  visualplatform['/api/photo/section/list'](...)
+
+The first parameter is always a JS object with the filter data described in 
+http://www.23developer.com/api/#methods
+
+The second and third parameters are callbacks in the event of success and
+error respectively.
+
+The client-side JavaScript library does not support OAuth authentication, 
+for this refer to the Node.js framework at http://github.com/23/node-23video
+
+This library requires jQuery.
+*/
+
+var Visualplatform = window.Visualplatform = (function($){
+    return function(domain){
+      var $api = this;
+      $api.serviceDomain = domain;
+      
+      /* API WEB SERVICE API */
+      $api.call = function(method, data, success, error){
+        // Handle arguments
+        data = data||{};
+        data['format'] = 'json';
+        $.ajax({
+            url:'http://'+$api.serviceDomain+'/'+method, 
+            data:data,
+            crossDomain:true, 
+            dataType:'jsonp', 
+            success:function(res) {
+              if(res.status == 'ok') {
+                if(success) success(res);
+              } else {
+                if(error) error(res.message);
+              }
+            },
+            error:function(err) {
+              if(error) error(err);
+            }
+          });
+      }
+      
+      // Map entire Visualplatform API
+      var methods = ['/api/analytics/report/event', '/api/analytics/report/play', '/api/album/list', '/api/comment/list', '/api/distribution/ios/register-device', '/api/distribution/ios/unregister-device', '/api/license/list', '/api/liveevent/list', '/api/liveevent/stream/list', '/api/photo/list', '/api/photo/rate', '/api/player/list', '/api/player/settings', '/api/photo/section/list', '/api/site/get', '/api/photo/subtitle/list', '/api/tag/list', '/api/tag/related', '/api/echo', '/api/user/list'];
+      
+      // Build functions for each Visualplatform API method
+      for (i in methods) {
+        var method = methods[i];
+        $api[method] = (function(method){
+            return function(data,sucess,error){
+              var data=data||{};
+              return($api.call(method, data, sucess, error));
+            }
+          })(method);
+        
+        // Create sub-objects for the different API namespaces
+        var camelizedMethod = method.replace(/-(.)/g, function(_,$1){return $1.toUpperCase()});
+        var s = camelizedMethod.split('/').slice(2);
+        var x = [];
+        for (var i=0; i<s.length-1; i++) {
+          x.push(s[i]);
+          if(!$api[x.join('.')]) $api[x.join('.')] = {};
+        }
+        // Create an alias for the method (both $api.album.list and $api['album.list'])
+        if(x.length>0) {
+          $api[x.join('.')][s[s.length-1]] = $api[method];
+        } else {
+          $api[s[s.length-1]] = $api[method];
+        }
+        $[s.join('.')] = $[method];
+      };
+      
+      return(this);
+    };
+
+  })(jQuery);
+
